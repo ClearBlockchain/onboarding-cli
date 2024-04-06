c89129006595367bbc8f44a3fe8629b6ba4237f8
Elon Salfati <elon@salfati.group>
2024-04-06 13:09:13 +0200

    typo

--- scripts/completions.sh
+++ scripts/completions.sh
@@ -3,5 +3,5 @@ set -e
 rm -rf completions
 mkdir completions
 for sh in bash zsh fish; do
-	go run main.go completion "$sh" >"completions/glide.$sh"
+	go run ./cmd/glide/main.go completion "$sh" >"completions/glide.$sh"
 done
